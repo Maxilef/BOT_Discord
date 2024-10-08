250fe025fef6a70440a2018386d1d00ff299e424
Maxilef <maxouroussel925@gmail.com>
2024-10-08 16:00:54 +0200

    ajout connection jpa bdd

---
+++ src/main/java/modele/APPUser.java
@@ -0,0 +1,29 @@
+package modele;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class APPUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String email;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
