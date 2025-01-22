09f66da6d55edb2a30434f970408a143b06b08c6
Maxilef <maxouroussel925@gmail.com>
2025-01-22 16:01:14 +0100

    save avant correction payara

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,21 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+<
+WEB/404.html,9/2/92183541179e4ab4294ac9d5a3c916e106e1c258
+N
+BDD/src/main/java/bdd/note.txt,4/9/49d44f564a8dbb045d51ab8a91fb73f7f7b2419e
+f
+6Rest/src/test/java/REST/rest/APPUserWithRolesTest.java,9/c/9cda3ed37491a4468f8d278750fa0db173e4350e
+Q
+!Rest/src/main/java/REST/Main.java,2/e/2e08615952df3c87afb46479617422c5848ac6f2
+P
+ Rest/src/main/java/REST/noteCurl,e/d/ed62a7dba8f39ce35dc630db7b697b0e485da272
+Z
+*BOT/src/main/java/com/example/bot/note.txt,5/4/5443bb411dca34164902cca11f2fbda6286af1e9
+D
+WEB/create_user.html,5/0/504da54f5e922cdd8e31bb8ca9fe419977542665
+>
+WEB/index.html,0/5/05c868ab148068532aaddc67cde5f18f72569baa
+^
+.BOT/src/main/java/com/example/bot/MainBot.java,c/5/c576365e702de9e235c45705be76a9be01a1fe7c
--- BOT/src/main/java/com/example/bot/MainBot.java
+++ BOT/src/main/java/com/example/bot/MainBot.java
@@ -12,9 +12,9 @@ public class MainBot extends ListenerAdapter {
 
         try {
             JDABuilder builder = JDABuilder.createDefault(token)
-                    .enableIntents(GatewayIntent.MESSAGE_CONTENT) // Active l'accès au contenu des messages
+                    .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS) // Active l'intention pour les membres
                     .addEventListeners(new MainBot())
-                    .setActivity(Activity.playing("!ping pour tester")); // Ajout d'une activité pour votre bot
+                    .setActivity(Activity.playing("!ping pour tester"));
 
             builder.build();
         } catch (Exception e) {
@@ -31,5 +31,37 @@ public class MainBot extends ListenerAdapter {
         if (event.getMessage().getContentRaw().equalsIgnoreCase("!ping")) {
             event.getChannel().sendMessage("Pong!").queue();
         }
+
+        // Commande !update
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!update")) {
+            // Vérifie si le message vient d'un serveur (Guild)
+            if (event.isFromGuild()) {
+                StringBuilder response = new StringBuilder("Liste des utilisateurs et leurs rôles :\n");
+
+                // Parcourt les membres du serveur
+                event.getGuild().loadMembers().onSuccess(members -> {
+                    for (var member : members) {
+                        // Ajoute le nom d'utilisateur et ses rôles au message
+                        response.append(member.getEffectiveName()).append(" : ");
+                        if (member.getRoles().isEmpty()) {
+                            response.append("Aucun rôle");
+                        } else {
+                            response.append(member.getRoles().stream()
+                                    .map(role -> role.getName())
+                                    .reduce((a, b) -> a + ", " + b).orElse("Aucun rôle"));
+                        }
+                        response.append("\n");
+                    }
+
+                    // Envoie la liste dans le canal
+                    event.getChannel().sendMessage(response.toString()).queue();
+                }).onError(e -> {
+                    event.getChannel().sendMessage("Une erreur s'est produite lors du chargement des membres.").queue();
+                });
+            } else {
+                event.getChannel().sendMessage("Cette commande doit être utilisée dans un serveur !").queue();
+            }
+        }
     }
+
 }
